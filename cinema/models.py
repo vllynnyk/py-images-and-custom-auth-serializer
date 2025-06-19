50be1435e774773cf9c5c417db1b5674d08049d0
vllynnyk <magnetto54@gmail.com>
2025-06-19 13:01:52 +0200

    Fix Migrations

--- cinema/models.py
+++ cinema/models.py
@@ -53,10 +53,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(
-        null=True,
-        upload_to="cinema.models.movie_image_path"
-    )
+    image = models.ImageField(null=True, upload_to=movie_image_path)
 
     class Meta:
         ordering = ["title"]
